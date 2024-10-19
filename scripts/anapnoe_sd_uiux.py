0bc1e11180a2eb338752ba009193ea0ff27215c9
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-10-20 00:49:21 +0300

    Set Dev Mode state to false by default

--- scripts/anapnoe_sd_uiux.py
+++ scripts/anapnoe_sd_uiux.py
@@ -109,9 +109,9 @@ shared.options_templates.update(
         ("anapnoe-ext", "Utils Extensions", "anapnoe"),
         {
             "uiux_enable_console_log": shared.OptionInfo(False, "Enable console log"),
-            "uiux_enable_dev_mode": shared.OptionInfo(True, "Enable Development Mode"),
+            "uiux_enable_dev_mode": shared.OptionInfo(False, "Enable Development Mode (Needs Restart)"),
             "uiux_enable_theme_editor": shared.OptionInfo(True, "Enable Theme Editor (Needs Restart)"),
-            "uiux_enable_event_delegation": shared.OptionInfo(False, "Enable Event Delegation for Extra Networks"),
+            "uiux_enable_event_delegation": shared.OptionInfo(False, "Enable Event Delegation for Extra Networks (Needs Restart)"),
             "uiux_max_resolution_output": shared.OptionInfo(
                 2048, "Max resolution output for txt2img and img2img"
             ),
