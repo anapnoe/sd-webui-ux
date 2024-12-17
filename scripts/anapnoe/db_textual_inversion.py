509a0005418f0c205968a255f18884127d50b342
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-12-17 08:55:17 +0200

    Update db_textual_inversion.py

--- scripts/anapnoe/db_textual_inversion.py
+++ scripts/anapnoe/db_textual_inversion.py
@@ -1,6 +1,7 @@
 import os
-
+from pathlib import Path
 from modules import ui_extra_networks, sd_hijack, shared
+
 
 
 class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
@@ -10,6 +11,15 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
 
     def refresh(self):
         sd_hijack.model_hijack.embedding_db.load_textual_inversion_embeddings(force_reload=True)
+    
+    def find_preview_image(self, path):
+        potential_files = sum([[f"{path}.{ext}", f"{path}.preview.{ext}"] for ext in ui_extra_networks.allowed_preview_extensions()], [])
+
+        for file in potential_files:
+            if self.lister.exists(file):
+                return file
+
+        return None
 
     def create_item(self, name, index=None, enable_filter=True):
         embedding = sd_hijack.model_hijack.embedding_db.word_embeddings.get(name)
@@ -20,12 +30,14 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
         mtime, ctime = self.lister.mctime(embedding.filename)
         hash = embedding.hash if embedding.hash else None
         stats = os.stat(embedding.filename)
+        preview_image = self.find_preview_image(path)
+        preview_path = Path(preview_image).as_posix() if preview_image else ""
 
         return {
             "name": (name, "TEXT"),
             "filename": (embedding.filename, "TEXT"),
             "hash": (hash, "TEXT"),
-            "preview": (self.find_preview(path), "TEXT"),
+            "preview": (preview_path, "TEXT"),
             "thumbnail": ("", "TEXT"),
             "description": (self.find_description(path), "TEXT"),
             "notes": ("", "TEXT"),
