96b2974c821225d4820a5a39f11b7c70a8ed241c
anapnoe <124302297+anapnoe@users.noreply.github.com>
2024-12-17 08:55:23 +0200

    Update db_textual_inversion_forge.py

--- scripts/anapnoe/db_textual_inversion_forge.py
+++ scripts/anapnoe/db_textual_inversion_forge.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import modules.textual_inversion.textual_inversion
 
 from modules.shared import cmd_opts
@@ -17,6 +18,15 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
 
     def refresh(self):
         embedding_db.load_textual_inversion_embeddings(force_reload=True, sync_with_sd_model=False)
+    
+    def find_preview_image(self, path):
+        potential_files = sum([[f"{path}.{ext}", f"{path}.preview.{ext}"] for ext in ui_extra_networks.allowed_preview_extensions()], [])
+
+        for file in potential_files:
+            if self.lister.exists(file):
+                return file
+
+        return None
 
     def create_item(self, name, index=None, enable_filter=True):
         embedding = embedding_db.word_embeddings.get(name)
@@ -27,12 +37,14 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
         mtime, ctime = self.lister.mctime(embedding.filename)
         hash = embedding.hash if embedding.hash else None
         stats = os.stat(embedding.filename)
+        preview_image = self.find_preview_image(path)
+        preview_path = Path(preview_image).as_posix() if preview_image else ""
     
         return {
             "name": (name, "TEXT"),
             "filename": (embedding.filename, "TEXT"),
             "hash": (hash, "TEXT"),
-            "preview": (self.find_preview(path), "TEXT"),
+            "preview": (preview_path, "TEXT"),
             "thumbnail": ("", "TEXT"),
             "description": (self.find_description(path), "TEXT"),
             "notes": ("", "TEXT"),
